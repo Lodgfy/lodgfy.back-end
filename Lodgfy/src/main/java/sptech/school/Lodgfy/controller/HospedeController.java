c8b5accd1d0a78ca61de3784a48c73dde5df036b
GuiSantos04 <guilherme.fsantos@sptech.school>
2025-09-02 20:15:14 -0300

    feature: Inclusão novos metodos

--- Lodgfy/src/main/java/sptech/school/Lodgfy/controller/HospedeController.java
+++ Lodgfy/src/main/java/sptech/school/Lodgfy/controller/HospedeController.java
@@ -24,10 +24,25 @@ public class HospedeController {
         return hospedeRepository.findAll();
     }
 
+    @GetMapping("/{cpf}")
+    public List<Hospede> getByNome(@PathVariable  String cpf) {
+        return hospedeRepository.findByCpf(cpf);
+    }
+
     @PostMapping
     public ResponseEntity<Hospede> createHospede(@Valid @RequestBody Hospede hospede) {
         Hospede savedHospede = hospedeRepository.save(hospede);
         return ResponseEntity.ok(savedHospede);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        if (hospedeRepository.existsById(id)) {
+            hospedeRepository.deleteById(id);
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.status(404).build();
+        }
     }
 
 }
--- Lodgfy/src/main/java/sptech/school/Lodgfy/repository/HospedeRepository.java
+++ Lodgfy/src/main/java/sptech/school/Lodgfy/repository/HospedeRepository.java
@@ -4,7 +4,10 @@ import sptech.school.Lodgfy.entity.Hospede;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HospedeRepository extends JpaRepository<Hospede, Long> {
 
+    List<Hospede> findByCpf(String cpf);
 }
