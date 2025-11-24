0181e031d820003118e1d409d2b7949cb6994724
GuiSantos04 <guilherme.fsantos@sptech.school>
2025-11-24 20:08:24 -0300

    feat: "exceptions personalizadas - reservas"

--- Lodgfy/src/main/java/sptech/school/Lodgfy/business/exceptions/ReservaConflitanteException.java
+++ Lodgfy/src/main/java/sptech/school/Lodgfy/business/exceptions/ReservaConflitanteException.java
@@ -1,12 +1,8 @@
 package sptech.school.Lodgfy.business.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class ReservaConflitanteException extends RuntimeException {
-    public ReservaConflitanteException(String message) {
-        super(message);
+    public ReservaConflitanteException() {
+        super("Já existe uma reserva confirmada para este chalé no período solicitado");
     }
 }
 
